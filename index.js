a0889eda57070336993c0a4e0dc9375e9ad41997
Sasha <2010kvadratik@gmail.com>
2025-11-14 15:12:27 +0200

    add update carousel functuion add styles

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-const track = document.querySelector(".carousel-track");
+const track = document.querySelector("#carousel-track");
+const slides = Array.from(track.children);
 const prevButton = document.querySelector("#btnPrev");
 const nextButton = document.querySelector("#btnNext");
+
+let currentIndex = 0;
+
+function updateCarousel() {
+  const slideWidth = slides[0].getBoundingClientRect().width;
+  track.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+}
+
+nextButton.addEventListener("click", () => {
+  currentIndex++;
+  if (currentIndex >= slides.length) {
+    currentIndex = 0;
+  }
+  updateCarousel();
+});
+
+prevButton.addEventListener("click", () => {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = slides.length - 1;
+  }
+  updateCarousel();
+});
